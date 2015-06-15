c22e36038204cab70ac6cd3ab658e2d28e246827
groob <vrancean+github@gmail.com>
2015-06-15 14:34:03 -0400

    added Do request and response

--- examples/main.go
+++ examples/main.go
@@ -2,17 +2,41 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
 
 	"github.com/groob/vcapi"
 )
 
 var (
-	username = "user"
-	password = "password"
+	username = "api.whitby"
+	password string
 )
 
+func init() {
+	password = os.Getenv("VCAPI_PASSWORD")
+	if password == "" {
+		log.Fatal("VCAPI_PASSWORD not set")
+	}
+
+}
+
 func main() {
-	config := &vcapi.Config{Username: username, Password: password, SchoolID: "whitby"}
+	config := &vcapi.Config{Username: username, Password: password, SchoolID: "whitby", APIVersion: "v1"}
 	client := vcapi.NewClient(config)
-	fmt.Println(client)
+	req, err := client.NewRequest("students.json")
+	fmt.Println(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(body))
 }
--- vcapi.go
+++ vcapi.go
@@ -61,9 +61,42 @@ func NewClient(config *Config) *Client {
 
 	baseURL, _ := url.Parse(defaultBaseURL)
 	// add Version and SchoolID to URL Path
-	baseURL.Path = config.APIVersion + "/" + config.SchoolID + "/"
+	baseURL.Path = config.SchoolID + "/" + config.APIVersion + "/"
 
 	c := &Client{client: http.DefaultClient, BaseURL: baseURL, UserAgent: userAgent, Config: config}
 
 	return c
+}
+
+// NewRequest creates an API request. A relative URL can be provided in urlStr, which will be resolved to the
+// BaseURL of the Client. Relative URLS should always be specified without a preceding slash.
+func (c *Client) NewRequest(urlStr string) (*http.Request, error) {
+	method := "GET"
+	rel, err := url.Parse(urlStr)
+	if err != nil {
+		return nil, err
+	}
+
+	u := c.BaseURL.ResolveReference(rel)
+
+	req, err := http.NewRequest(method, u.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.SetBasicAuth(c.Config.Username, c.Config.Password)
+
+	// req.Header.Add("Content-Type", mediaType)
+	req.Header.Add("Accept", mediaType)
+	req.Header.Add("User-Agent", userAgent)
+	return req, nil
+}
+
+// Do sends an API request and returns the API response.
+func (c *Client) Do(req *http.Request) (*http.Response, error) {
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
 }
